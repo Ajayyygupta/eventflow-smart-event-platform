c6cd3cf57efdcde6649f5bc3f7275900ce6f6dba
Ajay Gupta <ajju211503@gmail.com>
2026-05-11 02:45:34 -0700

    system and password encyption completed

--- src/main/java/com/eventflow/controller/HomeController.java
+++ src/main/java/com/eventflow/controller/HomeController.java
@@ -1,25 +1,65 @@
 package com.eventflow.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.eventflow.dto.LoginRequest;
 import com.eventflow.entity.User;
 import com.eventflow.service.UserService;
 
-@Controller
+@RestController
 @RequestMapping("/api/users")
 public class HomeController {
 
     @Autowired
     private UserService userService;
 
+    // REGISTER
+    // @PostMapping("/register")
+    // public User registeUser(@RequestBody User user) {
+    //     return userService.registerUser(user);
+    // }
+
+    //REGISTET (if email alreaady exist ke liye)
     @PostMapping("/register")
-    public User registeUser(@RequestBody User user) {
+    public ResponseEntity<?> registerUser(@RequestBody User user) {
+
+    try {
+        User savedUser = userService.registerUser(user);
+        return ResponseEntity.ok(savedUser);
+
+    } catch (RuntimeException e) {
+
+        return ResponseEntity
+                .badRequest()
+                .body(e.getMessage());
+    }
+   }
+
+
+    // LoGIN
+    @PostMapping("/login")
+    public String loginUser(@RequestBody LoginRequest request) {
         
-        return userService.registerUser(user);
+        return userService.loginUser(
+            request.getEmail(),
+            request.getPassword()
+        );
+        
+    }
+    
+
+    @GetMapping
+    public List<User> getAllUsers() {
+         return userService.getAllUsers();
     }
     
 
--- src/main/java/com/eventflow/service/UserService.java
+++ src/main/java/com/eventflow/service/UserService.java
@@ -1,6 +1,9 @@
 package com.eventflow.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.eventflow.entity.User;
@@ -12,15 +15,40 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
+    //Register
     public User registerUser(User user)
     {
+        if(userRepository.findByEmail(user.getEmail()).isPresent()){
+            throw new RuntimeException("Email already exist"); }
 
-        if(userRepository.findByEmail(user.getEmail()).isPresent())
-        {
-            throw new RuntimeException("Email already exist");
-        }
+            //Encrypt password
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
 
         return userRepository.save(user);
+    }
+
+    //Login
+    public String loginUser(String email, String password)
+    {
+        User user= userRepository.findByEmail(email)
+        .orElseThrow(() -> new RuntimeException("User not found"));
+
+        //Password checck
+        if(passwordEncoder.matches(password, user.getPassword())){
+
+            return "Login Succesfull";
+        }
+        throw new RuntimeException("Invalid password");
+
+    }
+
+
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
     }
 
 }
