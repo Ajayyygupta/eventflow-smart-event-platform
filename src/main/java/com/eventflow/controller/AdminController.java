99bf5ac2e98aa9cec89870714a6a552e2b38f68f
Ajay Gupta <ajju211503@gmail.com>
2026-05-21 15:28:40 -0700

    Added Add Event Buttonn in admin dashboard

--- src/main/java/com/eventflow/controller/AdminController.java
+++ src/main/java/com/eventflow/controller/AdminController.java
@@ -8,6 +8,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,6 +35,28 @@ public class AdminController {
     AdminController(EventRepository eventRepository) {
         this.eventRepository = eventRepository;
     }
+
+      //Add event
+      @PostMapping("/events")
+      public Event createEvent(@RequestBody Event event){
+
+       return eventRepository.save(event);
+       }
+
+       //Update event
+       @PutMapping("/events/{id}")
+       public Event updateEvent(@PathVariable Long id,@RequestBody Event updated){
+
+       Event event =eventRepository.findById(id).orElseThrow();
+
+        event.setTitle(updated.getTitle());
+        event.setLocation(updated.getLocation());
+        event.setDate(updated.getDate());
+        event.setCapacity(updated.getCapacity());
+        event.setDescription(updated.getDescription());
+
+        return eventRepository.save(event);
+     }
 
 
     //all users
